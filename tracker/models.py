afdf272dfb7f76f3c232928fc467ce3de6807dc7
arsha <ansariaarsh14@gmail.com>
2017-06-27 21:55:48 +0530

    able to post but the bus attribute(ID) is set to default and is not changing on posting

--- tracker/models.py
+++ tracker/models.py
@@ -15,8 +15,12 @@ class Location(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     ID = models.ForeignKey(Bus, null=True)
-    updated_time = models.DateTimeField(auto_now = True)
-
+    updated_time = models.DateTimeField(auto_now=True)
+    
+    def save(self, *args, **kwargs):
+        if self.ID is None:  # Set default reference
+            self.ID = Bus.objects.get(id=5)
+        super(Location, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self
