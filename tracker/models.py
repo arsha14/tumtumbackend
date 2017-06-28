6873f81c3c3de5bafb421ae96c906589724bc999
arsha <ansariaarsh14@gmail.com>
2017-06-29 05:24:08 +0530

    cleared migrations and database started new still getting errors

--- tracker/models.py
+++ tracker/models.py
@@ -11,17 +11,22 @@ class Bus(models.Model):
 class Location(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
-    bus = models.ForeignKey(Bus, default='')
+    bus = models.ForeignKey(Bus, null=True)
     updated_time = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
+        response = requests.get("http://127.0.0.1:8000/?format=json")
+        data = response.json()
         if kwargs.has_key('request'):
-            response = requests.get("http://127.0.0.1:8000/?format=json")
-            data = response.json()
             request = kwargs.pop('request')
             for addition in data:
                 if addition["bus"]["nfc_id"] == request.bus.nfc_id:
                     self.bus = Bus.objects.get(addition["id"])
+        else:
+            #self.bus = Bus.objects.get(data[0]["id"])   
+            super(Location, self).save(*args, **kwargs)     
+
+
             #request = kwargs.pop('request')
             #self.ID = request.ID
         #if req.ID is None:  # Set default referenc
